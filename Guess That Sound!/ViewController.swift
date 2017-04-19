36e0ad16095a6c36c4fd6ed965d220f2fba95bdf
Jason Liu <jase11554@yahoo.com>
2017-04-19 11:08:37 -0500

    Initial Commit

---
+++ Guess That Sound!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guess That Sound!
+//
+//  Created by jliu on 4/19/17.
+//  Copyright © 2017 Jason Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
