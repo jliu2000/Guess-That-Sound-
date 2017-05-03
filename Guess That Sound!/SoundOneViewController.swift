f43f2097618a8f6dd22a2423dae35131eeaa37a5
Sam Burrell <sburrell@Sams-MacBook-Air-4.local>
2017-05-03 11:03:59 -0500

    Created first sound with button

---
+++ Guess That Sound!/SoundOneViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SoundOneViewController.swift
+//  Guess That Sound!
+//
+//  Created by Sam Burrell on 5/3/17.
+//  Copyright © 2017 Jason Liu. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class SoundOneViewController: UIViewController {
+var audioBegin = AVAudioPlayer()
+    
+    var bankrupt = URL(fileURLWithPath: Bundle.main.path(forResource: "Bankrupt", ofType: "mp3")!)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func playSoundOne(_ sender: UIButton) {
+        do
+        {
+            try audioBegin = AVAudioPlayer(contentsOf: bankrupt, fileTypeHint: AVFileTypeMPEGLayer3)
+        }
+        catch
+        {
+            print("Error")
+        }
+        audioBegin.prepareToPlay()
+        audioBegin.play()
+    }
+    }
+
+
