62752fa5e48c4d894929768db9d9916570b2cff8
Sam Burrell <sburrell@Sams-MacBook-Air-4.local>
2017-05-04 11:05:05 -0500

    Fixed some bugs

---
+++ Guess That Sound!/SoundThreeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SoundThreeViewController.swift
+//  Guess That Sound!
+//
+//  Created by Sam Burrell on 5/4/17.
+//  Copyright © 2017 Jason Liu. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class SoundThreeViewController: UIViewController {
+    var audioBegin = AVAudioPlayer()
+    
+    var turbo = URL(fileURLWithPath: Bundle.main.path(forResource: "Turbo", ofType: "mp3")!)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+    @IBAction func playSoundThreeOnButtonTapped(_ sender: UIButton) {
+        
+        do
+        {
+            try audioBegin = AVAudioPlayer(contentsOf: turbo, fileTypeHint: AVFileTypeMPEGLayer3)
+        }
+        catch
+        {
+            print("Error")
+        }
+        audioBegin.prepareToPlay()
+        audioBegin.play()
+    }
+
+}
