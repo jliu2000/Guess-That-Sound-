501710720ee079bed3ee0c46b7f16e16e3e77190
Jason Liu <jase11554@yahoo.com>
2017-05-09 11:25:30 -0500

    5/9/17

--- Guess That Sound!/SoundOneViewController.swift
+++ Guess That Sound!/SoundOneViewController.swift
@@ -13,12 +13,27 @@ class SoundOneViewController: UIViewController {
     
     var audioBegin = AVAudioPlayer()
     var bankrupt = URL(fileURLWithPath: Bundle.main.path(forResource: "Bankrupt", ofType: "mp3")!)
+    var timer = Timer()
+    var  timeLeft = 20
+    var counter = 6
     
+    @IBOutlet weak var timerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    func timerRunning (){
+        timeLeft = -1
+        timerLabel.text = "Time Left"
+        if timeLeft <= 0{
+            timer.invalidate()
+            timerLabel.text = "Time's up"
+            
+            
+        }
+    }
+    
 
     @IBAction func playSoundOne(_ sender: UIButton) {
         
