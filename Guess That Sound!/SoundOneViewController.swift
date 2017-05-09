ca56b8b0a8a476fcdfb1ff42bb8f125956a67dba
Jason Liu <jase11554@yahoo.com>
2017-05-09 12:19:41 -0500

    Timer working

--- Guess That Sound!/SoundOneViewController.swift
+++ Guess That Sound!/SoundOneViewController.swift
@@ -14,25 +14,18 @@ class SoundOneViewController: UIViewController {
     var audioBegin = AVAudioPlayer()
     var bankrupt = URL(fileURLWithPath: Bundle.main.path(forResource: "Bankrupt", ofType: "mp3")!)
     var timer = Timer()
-    var  timeLeft = 20
+    var  timeLeft = 5
     var counter = 6
     
+    @IBOutlet weak var correctAnswer: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    func timerRunning (){
-        timeLeft = -1
-        timerLabel.text = "Time Left"
-        if timeLeft <= 0{
-            timer.invalidate()
-            timerLabel.text = "Time's up"
-            
-            
-        }
-    }
+    
+   
     
 
     @IBAction func playSoundOne(_ sender: UIButton) {
@@ -47,7 +40,19 @@ class SoundOneViewController: UIViewController {
         }
             audioBegin.prepareToPlay()
             audioBegin.play()
-        }
+        timeLeft = 5
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(SoundOneViewController.timerRunning), userInfo: nil, repeats: true)
         }
 
-
+func timerRunning (){
+    timeLeft -= 1
+    timerLabel.text = "Time Left: \(timeLeft)"
+    
+    if timeLeft == 0{
+        timer.invalidate()
+        timerLabel.text = "Time's up"
+        correctAnswer.isHidden = true
+        
+    }
+}
+}
