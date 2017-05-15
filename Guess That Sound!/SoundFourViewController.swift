2a6296f568b286ea4cb6601a706e96859b33a3dc
Jason Liu <jase11554@yahoo.com>
2017-05-15 17:07:28 -0500

    four done

--- Guess That Sound!/SoundFourViewController.swift
+++ Guess That Sound!/SoundFourViewController.swift
@@ -12,11 +12,19 @@ class SoundFourViewController: UIViewController {
     
     var audioBegin = AVAudioPlayer()
     var nailGun = URL(fileURLWithPath: Bundle.main.path(forResource: "NailGun", ofType: "mp3")!)
+    var timer = Timer()
+    var  timeLeft = 6
+    var counter = 6
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var correctAnswer: UIButton!
+    @IBOutlet weak var incorrectAnswer1: UIButton!
+    @IBOutlet weak var incorrectAnswer2: UIButton!
+    @IBOutlet weak var incorrectAnswer3: UIButton!
+    @IBOutlet weak var backToStart: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        backToStart.isHidden = true
     }
     
     @IBAction func playSoundFourOnButtonTapped(_ sender: UIButton) {
@@ -29,7 +37,25 @@ class SoundFourViewController: UIViewController {
         {
             print("Error")
         }
-            audioBegin.prepareToPlay()
-            audioBegin.play()
+        audioBegin.prepareToPlay()
+        audioBegin.play()
+       
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(SoundFourViewController.timerRunning), userInfo: nil, repeats: true)
+    }
+    
+    func timerRunning (){
+        timeLeft -= 1
+        timerLabel.text = "Time Left: \(timeLeft)"
+        
+        if timeLeft == 0{
+            timer.invalidate()
+            timerLabel.text = "Time's up"
+            correctAnswer.isHidden = true
+            incorrectAnswer1.isHidden = true
+            incorrectAnswer2.isHidden = true
+            incorrectAnswer3.isHidden = true
+            backToStart.isHidden = false
         }
-        }
+    }
+    
+}
